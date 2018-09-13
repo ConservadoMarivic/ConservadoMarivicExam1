29724a997f72be849759dff4f1b96c4edb644303
ConservadoMarivic <2015081245@ust-ics.mygbiz.com>
2018-09-13 20:27:27 +0800

    Initial commit

---
+++ app/src/main/java/marivic/conservado/com/conservadomarivicexam1/exam1.java
@@ -0,0 +1,13 @@
+package marivic.conservado.com.conservadomarivicexam1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class exam1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint);
+    }
+}
